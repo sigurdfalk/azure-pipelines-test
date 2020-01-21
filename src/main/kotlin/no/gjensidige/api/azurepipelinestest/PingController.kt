867040fedafdf4cf98c5fcd51dc9491cbca75546
Sigurd Hagen Falk <sigurd.falk@gjensidige.no>
2020-01-21 12:50:07 +0100

    Add boilerplate Spring Boot app with ping controller

---
+++ src/main/kotlin/no/gjensidige/api/azurepipelinestest/PingController.kt
@@ -0,0 +1,12 @@
+package no.gjensidige.api.azurepipelinestest
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("ping")
+class PingController {
+    @GetMapping
+    fun test() = "pong!"
+}
